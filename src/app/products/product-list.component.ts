9a8a7e5e358decc8f4eae254305ac37c8115568f
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-04-15 11:43:50 -0400

    mapping returned data -> transforming array elements

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EMPTY } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
@@ -13,6 +14,16 @@ export class ProductListComponent {
   categories;
 
   products$ = this.productService.products$.pipe(
+    map((products) =>
+      products.map(
+        (product) =>
+          ({
+            ...product,
+            price: product.price * 1.5,
+            searchKey: [product.productName],
+          } as Product)
+      )
+    ),
     catchError((error) => {
       this.errorMessage = error;
       return EMPTY;
