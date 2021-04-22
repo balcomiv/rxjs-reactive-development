7ad74fc14445cad1b5468d593ed732d3f1db6547
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-04-22 11:46:47 -0400

    update - reacting to actions -> filtering a stream

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
+  selectedCategoryId = 1; // Temporarily hardcoded until selector goes in
 
   products$ = this.productService.productsWithCategory$.pipe(
     map((products) =>
@@ -28,6 +29,16 @@ export class ProductListComponent {
       this.errorMessage = error;
       return EMPTY;
     })
+  );
+
+  productsSimpleFilter$ = this.productService.productsWithCategory$.pipe(
+    map((products) =>
+      products.filter((product) =>
+        this.selectedCategoryId
+          ? product.categoryId === this.selectedCategoryId
+          : true
+      )
+    )
   );
 
   constructor(private productService: ProductService) {}
